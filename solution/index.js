5f8ee64d5ea8c0cff929ceb634277d5e6638a4ee
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2021-09-23 15:39:21 +0300

    Add tasks
    Adding tasks now workes as intended and passes the test. Only the text of the task is now saved in localstorage, and not the whole element.

--- solution/index.js
+++ solution/index.js
@@ -1,19 +1,31 @@
 
 //Recieves the list id which the task should be created in, and creates the task.
 function addTask(listId) {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
     let text;
     //Finds the task's text by finding the input element of the section and taking its value.
     for (let child of document.getElementById(listId).parentElement.children) {
         if (child.tagName === "INPUT") {
             text = child.value;
-            child.value="";
+            child.value = "";
         }
     }
-    const id = createTaskId();
-    const taskObj = { type: "li", attributes: { id: id, "data-text": text, "data-parentId": listId }, classes: [] }
 
-    localStorage.setItem("task" + id, JSON.stringify({ taskObj }));
+    if (listId === "todo") {
+        tasks.todo.push(text);
+    }
+    if (listId === "in-progress") {
+        tasks['in-progress'].push(text);
+    }
+    if (listId === "done") {
+        tasks.done.push(text);
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     generateTasks();
+}
+
+function addToLocalStorage() {
+
 }
 
 //Recieves an object containing the element type, attributes, and classes, and makes an html element from it.
@@ -28,28 +40,42 @@ function createElementFromObject(obj) {
     return el;
 }
 
-//Creates a new unique id for the task
-function createTaskId() {
-    if (localStorage.length === 0) {
-        return 1;
-    }
-    return localStorage.length + 1;
+function createTaskId(tasks) {
+    return tasks.todo.length + tasks['in-progress'].length + tasks.done.length + 1;
 }
 
 //generates all the tasks by appending their li element to the corresponding ul element.
 function generateTasks() {
+    if (localStorage.length === 0) {
+        const tasksObj = {
+            "todo": [],
+            "in-progress": [],
+            "done": []
+        }
+        localStorage.setItem("tasks", JSON.stringify(tasksObj))
+    }
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
     //First, resets all the ul elements to prevent duplication
-    document.getElementById("toDoTasks").textContent="";
-    document.getElementById("inProgressTasks").textContent="";
-    document.getElementById("doneTasks").textContent="";
+    document.getElementById("todo").textContent = "";
+    document.getElementById("in-progress").textContent = "";
+    document.getElementById("done").textContent = "";
     //Goes through localStorage, creates the li elements, and appends it to the right ul.
-    for (let i = 1; i <= localStorage.length; i++) {
-        const el = createElementFromObject(JSON.parse(localStorage.getItem("task" + i)).taskObj);
-        el.textContent = el.getAttribute("data-text");
-        console.log(el)
-        const parent = document.getElementById(el.getAttribute("data-parentId"));
+    appendTask(tasks, "todo");
+    appendTask(tasks, "in-progress");
+    appendTask(tasks, "done");
+}
 
-        parent.append(el);
+function appendTask(tasks, taskList) {
+    if (tasks[taskList].length > 0) {
+        for (let i = 0; i < tasks[taskList].length; i++) {
+            const id = createTaskId(tasks);
+            const taskObj = { type: "li", attributes: { id: id }, classes: ["task"] }
+            const el = createElementFromObject(taskObj);
+            el.textContent = tasks[taskList][i];
+            const parent = document.getElementById(taskList);
+
+            parent.append(el);
+        }
     }
 }
 
